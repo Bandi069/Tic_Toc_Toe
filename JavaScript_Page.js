42bcc5d301c7f4745db58b10da939446607c8701
Bandi069 <bandivenu89@gmail.com>
2020-07-12 11:41:40 +0530

    Modified JS file

--- JavaScript_Page.js
+++ JavaScript_Page.js
@@ -15,4 +15,13 @@ $('table tr td').click(function(){
 });
 function findWinner(){
 	player1=$('table tr:nth-child(1) td:nth-child(1)');
+player1=$('table tr:nth-child(1) td:nth-child(1)');
+player2=$('table tr:nth-child(1) td:nth-child(2)');
+player3=$('table tr:nth-child(1) td:nth-child(3)');
+player4=$('table tr:nth-child(2) td:nth-child(1)');
+player5=$('table tr:nth-child(2) td:nth-child(2)');
+player6=$('table tr:nth-child(2) td:nth-child(3)');
+player7=$('table tr:nth-child(3) td:nth-child(1)');
+player8=$('table tr:nth-child(3) td:nth-child(2)');
+player9=$('table tr:nth-child(3) td:nth-child(3)');
 }
