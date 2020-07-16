2c6ba5f8a90a0d2554ca2554a7103368dce43af9
Bandi069 <bandivenu89@gmail.com>
2020-07-16 21:37:14 +0530

    Final code commit

--- JavaScript_Page.js
+++ JavaScript_Page.js
@@ -1,27 +1,78 @@
-let move=1;
-let play=true;
-
-$('table tr td').click(function(){
-	if($(this).text()==""&& play){
-		if((move%2)==1){
+let move = 1;
+let play = true;
+var gameWinnerId = document.getElementById("gameWinner");
+$('table tr td').click(function () {
+	if ($(this).text() == "" && play) {
+		if ((move % 2) == 1) {
 			$(this).append('X');
-			$(this).css('color','#61892f');
-		}else{
+			$(this).css('color', 'green');
+		} else {
 			$(this).append('O');
-			$(this).css('color','#e85a4f');
+			$(this).css('color', 'red');
 		}
 		move++;
+		if (findWinner() != -1 && findWinner() != "") {
+			if (findWinner() == "X") {
+				$('body').append('<div class="winner"><span>Winner</span>X</div>');
+				$('body').append('<button onclick="location.reload()">Play Again</div>');
+				$('.winner').css('background-color', 'green');
+				$('button').css('color', 'green');
+			} 
+			// else if (findWinner() == -1 ) {
+			// 	$('body').append('<div class="winner"><span>Match drawn</span></div>');
+			// 	$('body').append('<button onclick="location.reload()">Play Again</div>');
+			// 	$('.winner').css('background-color', 'white');
+			// 	$('button').css('color', 'gold');
+			// }
+			else {
+				$('body').append('<div class="winner"><span>Winner</span>O</div>');
+				$('body').append('<button onclick="location.reload()">Play Again</div>');
+				$('.winner').css('background-color', 'red');
+				$('button').css('color', 'red');
+			}
+			play = false;
+		}
 	}
 });
-function findWinner(){
-	player1=$('table tr:nth-child(1) td:nth-child(1)');
-player1=$('table tr:nth-child(1) td:nth-child(1)');
-player2=$('table tr:nth-child(1) td:nth-child(2)');
-player3=$('table tr:nth-child(1) td:nth-child(3)');
-player4=$('table tr:nth-child(2) td:nth-child(1)');
-player5=$('table tr:nth-child(2) td:nth-child(2)');
-player6=$('table tr:nth-child(2) td:nth-child(3)');
-player7=$('table tr:nth-child(3) td:nth-child(1)');
-player8=$('table tr:nth-child(3) td:nth-child(2)');
-player9=$('table tr:nth-child(3) td:nth-child(3)');
+
+function findWinner() {
+	player1 = $('table tr:nth-child(1) td:nth-child(1)').text();
+	player2 = $('table tr:nth-child(1) td:nth-child(2)').text();
+	player3 = $('table tr:nth-child(1) td:nth-child(3)').text();
+	player4 = $('table tr:nth-child(2) td:nth-child(1)').text();
+	player5 = $('table tr:nth-child(2) td:nth-child(2)').text();
+	player6 = $('table tr:nth-child(2) td:nth-child(3)').text();
+	player7 = $('table tr:nth-child(3) td:nth-child(1)').text();
+	player8 = $('table tr:nth-child(3) td:nth-child(2)').text();
+	player9 = $('table tr:nth-child(3) td:nth-child(3)').text();
+
+	
+	if ((player1 == player2) && (player2 == player3)) {
+		return player3;
+	}
+	else if ((player4 == player5) && (player5 == player6)) {
+		return player6;
+	}
+	else if ((player7 == player8) && (player8 == player9)) {
+		return player9;
+	}
+	else if ((player1 == player4) && (player4 == player7)) {
+		return player7;
+	}
+	else if ((player2 == player5) && (player5 == player8)) {
+		return player8;
+	}
+	else if ((player3 == player6) && (player6 == player9)) {
+		return player9;
+	}
+	else if ((player1 == player5) && (player9 == player5)) {
+		return player9;
+	}
+	else if ((player3 == player5) && (player5 == player7)) {
+		return player7;
+	}
+	else {
+		return -1;//game draw
+	}
 }
+
